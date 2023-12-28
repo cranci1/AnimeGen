a86ac116ea48941812ee9c509d2902eb827aeb21
SoFranci <trevisanfrancesco022@gmail.com>
2023-12-28 11:25:46 +0100

    Initial Commit

---
+++ pic-re/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pic-re
+//
+//  Created by Francesco on 28/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
