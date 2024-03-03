d1eac5db5dece3fc0908a6777d53bfe6f5451a03
SoFranci <100066266+cranci1@users.noreply.github.com>
2024-03-03 17:45:04 +0100

    fix

--- AnimeGen/Settings/AboutPage.swift
+++ AnimeGen/Settings/AboutPage.swift
@@ -45,7 +45,7 @@ struct AboutPage: View {
                     }
                 }
                 
-                Section(header: Text("Privacy"), footer: Text("AnimeGen prioritizes user privacy and does not store any personal data. All generated images are not retained by the app. If a user chooses to save an image, it will only be stored locally in their device's gallery exept for the Hmtai APi. We are committed to ensuring a secure and private experience for our users.")) {
+                Section(header: Text("Privacy"), footer: Text("AnimeGen prioritizes user privacy and does not store any personal data. All generated images are not retained by the app. If a user chooses to save an image, it will only be stored locally in their device's gallery exept for the Hmtai API. We are committed to ensuring a secure and private experience for our users.")) {
                     
                     HStack {
                         Text("Review the code")
@@ -57,7 +57,7 @@ struct AboutPage: View {
                     }
                     
                     HStack {
-                        Text("Understand More for Hmtai")
+                        Text("Hmtai API Privacy")
                     }
                     .onTapGesture {
                         if let url = URL(string: "https://github.com/cranci1/AnimeGen/Privacy/Hmtai.md") {
