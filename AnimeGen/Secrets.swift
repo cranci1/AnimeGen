783676680bec94b65d16b25b7862129138a937b5
SoFranci <100066266+cranci1@users.noreply.github.com>
2024-03-30 17:24:18 +0100

    aaded secrets.swift for succesful build

---
+++ AnimeGen/Secrets.swift
@@ -0,0 +1,17 @@
+//
+//  Secrets.swift
+//  AnimeGen
+//
+//  Created by cranci on 03/03/24.
+//
+
+import Foundation
+
+struct Secrets {
+    static let apiToken = "Bot TokenHEREHEREHERE"
+    
+    static let discordWebhookURL = URL(string: "YourWebhookUrl")!
+    
+    static let discordChannelId = "YourChannelIdHere"
+}
+
