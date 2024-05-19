d13a39df313d1a9db0d575528e3a1066c6f297a8
cranci1 <trevisanfrancesco022@gmail.com>
2024-05-19 10:29:57 +0200

    Revert "test"
    This reverts commit 6f27de4ae11218e3d4a11d33bbcb4e15ff2044e8.

--- AnimeGen/Buttons func/HeartButton.swift
+++ AnimeGen/Buttons func/HeartButton.swift
@@ -13,7 +13,6 @@ extension ViewController {
 
     @IBAction func heartButtonTapped() {
         guard let image = imageView.image else {
-            print("No image found in imageView")
             return
         }
 
@@ -22,25 +21,27 @@ extension ViewController {
            let utType = CGImageSourceGetType(source),
            UTTypeConformsTo(utType, kUTTypeGIF) {
 
-            PHPhotoLibrary.shared().performChanges({
+            PHPhotoLibrary.shared().performChanges {
                 let creationRequest = PHAssetCreationRequest.forAsset()
                 creationRequest.addResource(with: .photo, data: data, options: nil)
-            }) { (success, error) in
-                DispatchQueue.main.async {
-                    if success {
-                        print("GIF image saved to Photos library")
-                        self.animateFeedback()
-                    } else {
-                        self.handleSaveError(error, isGIF: true)
-                    }
+            } completionHandler: { (success, error) in
+                if success {
+                    print("GIF image saved to Photos library")
+                    self.animateFeedback()
+                } else {
+                    print("Error saving GIF image: \(error?.localizedDescription ?? "")")
+                    self.showAlert(withTitle: "Error Saving Image!", message: "You didn't allow AnimeGen to access the Photo Library.", viewController: self)
                 }
             }
             return
         }
 
         if let imageData = image.jpegData(compressionQuality: 1.0),
-           let uiImage = UIImage(data: imageData) {
-            UIImageWriteToSavedPhotosAlbum(uiImage, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+            let uiImage = UIImage(data: imageData) {
+
+            DispatchQueue.main.async {
+                UIImageWriteToSavedPhotosAlbum(uiImage, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+            }
         } else {
             print("Error converting image to JPEG format")
         }
@@ -49,17 +50,10 @@ extension ViewController {
     @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error = error {
             print("Error saving image: \(error.localizedDescription)")
-            showAlert(withTitle: "Error Saving Image!", message: "You didn't allow AnimeGen to access the Photo Library.", viewController: self)
+            self.showAlert(withTitle: "Error Saving Image!", message: "You didn't allow AnimeGen to access the Photo Library.", viewController: self)
         } else {
-            animateFeedback()
+            self.animateFeedback()
             print("Image saved successfully")
         }
-    }
-
-    private func handleSaveError(_ error: Error?, isGIF: Bool) {
-        let imageType = isGIF ? "GIF" : "image"
-        let errorMessage = error?.localizedDescription ?? "Unknown error"
-        print("Error saving \(imageType): \(errorMessage)")
-        showAlert(withTitle: "Error Saving \(imageType.capitalized)!", message: "You didn't allow AnimeGen to access the Photo Library.", viewController: self)
     }
 }
