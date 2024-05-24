5c29e004d408499ffc7ffd8031e3234a0faadf35
cranci1 <trevisanfrancesco022@gmail.com>
2024-05-24 17:20:16 +0200

    sds

--- AnimeGen/Settings/SettingsMain.swift
+++ AnimeGen/Settings/SettingsMain.swift
@@ -13,23 +13,32 @@ class SettingsMain: UITableViewController {
     @IBOutlet weak var StepperWidth: UIStepper!
     @IBOutlet weak var StepperHeight: UIStepper!
     
-    var imageWidth: Double = 100.0
-    var imageHeight: Double = 60.0
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initialize steppers with singleton values
+        StepperWidth.value = Settings.shared.imageWidth
+        StepperHeight.value = Settings.shared.imageHeight
+        
+        // Set minimum and maximum values if needed
+        StepperWidth.minimumValue = 50.0
+        StepperWidth.maximumValue = 300.0
+        StepperHeight.minimumValue = 50.0
+        StepperHeight.maximumValue = 300.0
+        
+        // Add target actions for steppers
         StepperWidth.addTarget(self, action: #selector(widthStepperChanged(_:)), for: .valueChanged)
         StepperHeight.addTarget(self, action: #selector(heightStepperChanged(_:)), for: .valueChanged)
-      }
-    
-    @IBAction func widthStepperChanged(_ sender: UIStepper) {
-        imageWidth = sender.value
     }
     
-    @IBAction func heightStepperChanged(_ sender: UIStepper) {
-        imageHeight = sender.value
+    @objc func widthStepperChanged(_ sender: UIStepper) {
+        Settings.shared.imageWidth = sender.value
+        print("New imageWidth: \(Settings.shared.imageWidth)")
+    }
+    
+    @objc func heightStepperChanged(_ sender: UIStepper) {
+        Settings.shared.imageHeight = sender.value
+        print("New imageHeight: \(Settings.shared.imageHeight)")
     }
     
     @IBAction func APIsStatus(_ sender: UITapGestureRecognizer) {
