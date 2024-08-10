9fa484c870539c1830298d04782c6c6f3b5e1ec7
cranci1 <trevisanfrancesco022@gmail.com>
2024-08-10 09:36:58 +0200

    test

--- AnimeGen/Sources/SourceMenu.swift
+++ AnimeGen/Sources/SourceMenu.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SourceMenu {
     static weak var delegate: SourceSelectionDelegate?
-
+    
     static func showSourceSelector(from viewController: UIViewController, sourceView: UIView?) {
         DispatchQueue.main.async {
             let sources: [(title: String, source: MediaSource)] = [
@@ -23,71 +23,71 @@ class SourceMenu {
                 ("Anix", .anix)
             ]
             
+            let alertController: UIAlertController
+            
             if UIDevice.current.userInterfaceIdiom == .pad {
-                showPopoverMenu(from: viewController, sourceView: sourceView, sources: sources)
+                // Use popover style for iPad
+                alertController = UIAlertController(title: "Select Source", message: "Choose your preferred source for AnimeLounge.", preferredStyle: .alert)
+                
+                let stackView = UIStackView()
+                stackView.axis = .vertical
+                stackView.spacing = 10
+                stackView.distribution = .fillEqually
+                
+                for (title, source) in sources {
+                    let button = UIButton(type: .system)
+                    button.setTitle(title, for: .normal)
+                    button.addTarget(self, action: #selector(sourceButtonTapped(_:)), for: .touchUpInside)
+                    button.tag = sources.firstIndex(where: { $0.source == source }) ?? 0
+                    setSourceImage(for: button, named: title)
+                    stackView.addArrangedSubview(button)
+                }
+                
+                let contentViewController = UIViewController()
+                contentViewController.view = stackView
+                contentViewController.preferredContentSize = CGSize(width: 250, height: CGFloat(sources.count) * 50)
+                
+                alertController.setValue(contentViewController, forKey: "contentViewController")
             } else {
-                showActionSheet(from: viewController, sourceView: sourceView, sources: sources)
+                // Use action sheet style for iPhone
+                alertController = UIAlertController(title: "Select Source", message: "Choose your preferred source for AnimeLounge.", preferredStyle: .actionSheet)
+                
+                for (title, source) in sources {
+                    let action = UIAlertAction(title: title, style: .default) { _ in
+                        UserDefaults.standard.set(source.rawValue, forKey: "selectedMediaSource")
+                        delegate?.didSelectNewSource()
+                    }
+                    setSourceImage(for: action, named: title)
+                    alertController.addAction(action)
+                }
             }
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
+            if let popoverController = alertController.popoverPresentationController {
+                if let sourceView = sourceView, sourceView.window != nil {
+                    popoverController.sourceView = sourceView
+                    popoverController.sourceRect = sourceView.bounds
+                } else {
+                    popoverController.sourceView = viewController.view
+                    popoverController.sourceRect = viewController.view.bounds
+                }
+            }
+            
+            viewController.present(alertController, animated: true)
         }
     }
     
-    private static func showActionSheet(from viewController: UIViewController, sourceView: UIView?, sources: [(title: String, source: MediaSource)]) {
-        let alertController = UIAlertController(title: "Select Source", message: "Choose your preferred source for AnimeLounge.", preferredStyle: .actionSheet)
-        
-        for (title, source) in sources {
-            let action = UIAlertAction(title: title, style: .default) { _ in
-                UserDefaults.standard.selectedMediaSource = source
-                delegate?.didSelectNewSource()
-            }
-            setSourceImage(for: action, named: title)
-            alertController.addAction(action)
+    @objc private static func sourceButtonTapped(_ sender: UIButton) {
+        let sources: [MediaSource] = [.animeWorld, .gogoanime, .animeheaven, .animefire, .kuramanime, .jkanime, .anime3rb, .anix]
+        guard sender.tag < sources.count else { return }
+        let selectedSource = sources[sender.tag]
+        UserDefaults.standard.set(selectedSource.rawValue, forKey: "selectedMediaSource")
+        delegate?.didSelectNewSource()
+        sender.superview?.superview?.superview?.superview?.removeFromSuperview()
+        if let alertController = UIApplication.shared.windows.first?.rootViewController?.presentedViewController as? UIAlertController {
+            alertController.dismiss(animated: true, completion: nil)
         }
-        
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        
-        if let popoverController = alertController.popoverPresentationController {
-            if let sourceView = sourceView, sourceView.window != nil {
-                popoverController.sourceView = sourceView
-                popoverController.sourceRect = sourceView.bounds
-            } else {
-                popoverController.sourceView = viewController.view
-                popoverController.sourceRect = viewController.view.bounds
-            }
-        }
-        
-        viewController.present(alertController, animated: true)
-    }
-    
-    private static func showPopoverMenu(from viewController: UIViewController, sourceView: UIView?, sources: [(title: String, source: MediaSource)]) {
-        let tableViewController = UITableViewController(style: .plain)
-        tableViewController.tableView.isScrollEnabled = false
-        tableViewController.preferredContentSize = CGSize(width: 250, height: CGFloat(sources.count * 44))
-        
-        tableViewController.tableView.dataSource = tableViewController
-        tableViewController.tableView.delegate = tableViewController
-        
-        tableViewController.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SourceCell")
-        
-        let popoverController = tableViewController.popoverPresentationController
-        popoverController?.delegate = tableViewController as? UIPopoverPresentationControllerDelegate
-        popoverController?.permittedArrowDirections = .any
-        
-        if let sourceView = sourceView, sourceView.window != nil {
-            popoverController?.sourceView = sourceView
-            popoverController?.sourceRect = sourceView.bounds
-        } else {
-            popoverController?.sourceView = viewController.view
-            popoverController?.sourceRect = viewController.view.bounds
-        }
-        
-        tableViewController.tableView.dataSource = TableViewDataSource(sources: sources)
-        tableViewController.tableView.delegate = TableViewDelegate(sources: sources) { selectedSource in
-            UserDefaults.standard.selectedMediaSource = selectedSource
-            delegate?.didSelectNewSource()
-            tableViewController.dismiss(animated: true)
-        }
-        
-        viewController.present(tableViewController, animated: true)
     }
     
     private static func setSourceImage(for action: UIAlertAction, named imageName: String) {
@@ -95,43 +95,15 @@ class SourceMenu {
         let resizedImage = originalImage.resized(to: CGSize(width: 35, height: 35))
         action.setValue(resizedImage.withRenderingMode(.alwaysOriginal), forKey: "image")
     }
+    
+    private static func setSourceImage(for button: UIButton, named imageName: String) {
+        guard let originalImage = UIImage(named: imageName) else { return }
+        let resizedImage = originalImage.resized(to: CGSize(width: 35, height: 35))
+        button.setImage(resizedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 10)
+    }
 }
 
 protocol SourceSelectionDelegate: AnyObject {
     func didSelectNewSource()
-}
-
-class TableViewDataSource: NSObject, UITableViewDataSource {
-    private let sources: [(title: String, source: MediaSource)]
-    
-    init(sources: [(title: String, source: MediaSource)]) {
-        self.sources = sources
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sources.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SourceCell", for: indexPath)
-        let source = sources[indexPath.row]
-        cell.textLabel?.text = source.title
-        cell.imageView?.image = UIImage(named: source.title)?.resized(to: CGSize(width: 35, height: 35))
-        return cell
-    }
-}
-
-class TableViewDelegate: NSObject, UITableViewDelegate {
-    private let sources: [(title: String, source: MediaSource)]
-    private let onSelection: (MediaSource) -> Void
-    
-    init(sources: [(title: String, source: MediaSource)], onSelection: @escaping (MediaSource) -> Void) {
-        self.sources = sources
-        self.onSelection = onSelection
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedSource = sources[indexPath.row].source
-        onSelection(selectedSource)
-    }
 }
